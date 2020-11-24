fe61ad0ca70faa6d89dd97f65c7965d0799cef39
luowailin <blazer402@126.com>
2020-11-24 09:59:02 +0800

    camera writer

--- CameraWriter/CameraWriter/ViewController.m
+++ CameraWriter/CameraWriter/ViewController.m
@@ -6,8 +6,19 @@
 //
 
 #import "ViewController.h"
+#import "OverlayView.h"
+#import <GLKit/GLKit.h>
+#import "CameraController.h"
+#import "PreviewView.h"
+#import "ContextManager.h"
+#import "OverlayView.h"
+#import "PhotoFilters.h"
 
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet OverlayView *overlayView;
+@property (strong, nonatomic) CameraController *controller;
+@property (strong, nonatomic) PreviewView *previewView;
 
 @end
 
@@ -15,10 +26,41 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+    self.controller = [[CameraController alloc] init];
+    
+    CGRect frame = self.view.bounds;
+    EAGLContext *eaglContext = [ContextManager sharedInstance].eaglContext;
+    self.previewView = [[PreviewView alloc] initWithFrame:frame
+                                                  context:eaglContext];
+    
+    self.previewView.filter = [PhotoFilters defaultFilter];
+    
+    self.controller.imageTarget = self.previewView;
+    
+    self.previewView.coreImageContext = [ContextManager sharedInstance].ciContext;
+    [self.view insertSubview:self.previewView belowSubview:self.overlayView];
+    
+    NSError *error;
+    if ([self.controller setupSession:&error]) {
+        [self.controller startSession];
+    } else {
+        NSLog(@"Error: %@", [error localizedDescription]);
+    }
 }
 
 - (IBAction)captureOrRecord:(UIButton *)sender {
+    if (!self.controller.isRecording) {
+        dispatch_async(dispatch_queue_create("com.tapharmonic.camera", NULL), ^{
+            [self.controller startRecording];
+        });
+    } else {
+        [self.controller stopRecording];
+    }
+}
+
+- (BOOL)prefersStatusBarHidden{
+    return YES;
 }
 
 @end
