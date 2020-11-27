a32ba935d78643b2a68b1b4d182a1096f9bc1ab0
luowailin <blazer402@126.com>
2020-11-27 15:29:05 +0800

    15 seconds

---
+++ 15Seconds/15Seconds/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  15Seconds
+//
+//  Created by luowailin on 2020/11/25.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 15Seconds/15Seconds/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  15Seconds
+//
+//  Created by luowailin on 2020/11/25.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
